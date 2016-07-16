2d2f4f5bb4a49534abad66c4866d1b77b1f6cee5
fionster-fish-1 <fiontan@gmx.co.uk>
2016-07-16 09:25:34 +0100

    Merged StaffChat into WolfEssentials
    Used new staffchat methods

--- src/me/fionster_fish_1/wessentials/WEssentials.java
+++ src/me/fionster_fish_1/wessentials/WEssentials.java
@@ -874,7 +874,7 @@ public class WEssentials extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void ChatFilter(AsyncPlayerChatEvent e) {
+    public void onChat(AsyncPlayerChatEvent e) {
         Player player = e.getPlayer();
         String message = e.getMessage();
         String format = e.getFormat();
@@ -896,6 +896,46 @@ public class WEssentials extends JavaPlugin implements Listener {
                 e.setMessage("I LOVE WOLFPRISON!!!");
             }
         }
+
+        if (player.hasPermission("wolfprison.chat.send")) {
+            if (!(message.startsWith("@")))
+                return;
+            if (message.equalsIgnoreCase("@")) {
+                player.sendMessage(ChatColor.RED + "Invalid syntax, you have to send a message after the '@'");
+                return;
+            } else {
+                e.setCancelled(true);
+                String send = message.substring(1);
+                for (Player target : Bukkit.getOnlinePlayers()) {
+                    if (target.hasPermission("wolfprison.chat.view")) {
+                        target.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                "&4&lStaff&9&lChat &8> " + getRank(player) + "&5&o" + player.getName() + "&8: &7" + send));
+                    }
+                }
+            }
+        }
+    }
+    private String getRank(Player p) {
+        String wolfEssentials = "wolfessentials.rank.";
+        if (p.hasPermission(wolfEssentials + "trainee"))
+            return "&8&l[&3&lTrainee&8&l]&r ";
+        if (p.hasPermission(wolfEssentials + "helper"))
+            return "&8&l[&5&lHelper&8&l] ";
+        if (p.hasPermission(wolfEssentials + "mod"))
+            return "&8&l[&1&lModerator&8&l] ";
+        if (p.hasPermission(wolfEssentials + "admin"))
+            return "&8&l[&6&lAdmin&8&l] ";
+        if (p.hasPermission(wolfEssentials + "builder"))
+            return "&8&l[&3&lBuilder&8&l] ";
+        if (p.hasPermission(wolfEssentials + "co"))
+            return "&8&l[&4&lCo-Owner&8&l] ";
+        if (p.hasPermission(wolfEssentials + "dev"))
+            return "&8&l[&4&lDev&8&l] ";
+        if (p.hasPermission(wolfEssentials + "manager"))
+            return "&8&l[&c&lManager&8&l] ";
+        if (p.hasPermission(wolfEssentials + "owner"))
+            return "&8&l[&c&lOwner&8&l] ";
+        return "";
     }
 
     @EventHandler
